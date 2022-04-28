333f2bbe8228c63b87e785e9420024783da7a39a
José Urbina <j.urbina.0183@gmail.com>
2022-04-28 19:24:55 -0300

    update pass conexion db

--- model/conexion.php
+++ model/conexion.php
@@ -3,7 +3,7 @@
         static function connect() {
             $link = new PDO('mysql:host=localhost;dbname=pos',
                             'root', 
-                            'admroot13141516');
+                            'adminroot13141516');
 
             $link->exec('set names utf8');
 
