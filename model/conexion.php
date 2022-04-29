f6556f3090e6b4227a07bca7439766e737f0d1b9
José Urbina <j.urbina.0183@gmail.com>
2022-04-29 08:51:35 -0300

    logs 10 db

--- model/conexion.php
+++ model/conexion.php
@@ -5,6 +5,7 @@
             $link = new PDO('mysql:host=localhost;dbname=pos',
                             'root', 
                             'adminroot13141516');
+            $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             $link->exec('set names utf8');
 
