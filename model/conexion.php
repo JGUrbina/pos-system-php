4140b8fb9aeadfb788bf51070a1d19f0f70cc7b3
José Urbina <j.urbina.0183@gmail.com>
2022-05-04 18:47:28 -0300

    add module clients 2

--- model/conexion.php
+++ model/conexion.php
@@ -1,12 +1,12 @@
 <?php
     class Conexion {
         static function connect() {
-            // $link = new PDO('mysql:host=localhost;dbname=pos',
-            //                 'root', 
-            //                 'adminroot13141516');
             $link = new PDO('mysql:host=localhost;dbname=pos',
                             'root', 
-                            '');
+                            'adminroot13141516');
+            // $link = new PDO('mysql:host=localhost;dbname=pos',
+            //                 'root', 
+            //                 '');
 $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $link->exec('set names utf8');
 
